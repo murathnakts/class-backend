33abccd69f7d1f261f07a8538a39a74080bb8104
murathnakts <murathanaktas111002@gmail.com>
2025-09-08 10:17:05 +0300

    Initial commit

---
+++ src/main/java/com/murathnakts/starter/ClassBackendApplication.java
@@ -0,0 +1,13 @@
+package com.murathnakts.starter;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassBackendApplication.class, args);
+	}
+
+}
